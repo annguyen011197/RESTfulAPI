6ff528d69f87ecd224a9f6492fb52104d5a4cbf6
An Nguyen <tranminhannguyen@gmail.com>
2019-10-30 17:49:57 +0700

    remove duplicate

--- helper/Helper.js
+++ helper/Helper.js
@@ -2,40 +2,40 @@ function optional(obj, evalFunc, def) {
 
     // Our proxy handler
     const handler = {
-      // Intercept all property access
-      get: function(target, prop, receiver) {
-        const res = Reflect.get(...arguments);
-  
-        // If our response is an object then wrap it in a proxy else just return
-        return typeof res === "object" ? proxify(res) : res != null ? res : def;
-      }
+        // Intercept all property access
+        get: function (target, prop, receiver) {
+            const res = Reflect.get(...arguments);
+
+            // If our response is an object then wrap it in a proxy else just return
+            return typeof res === "object" ? proxify(res) : res != null ? res : def;
+        }
     };
-  
+
     const proxify = target => {
-      return new Proxy(target, handler);
+        return new Proxy(target, handler);
     };
-  
+
     // Call function with our proxified object
     return evalFunc(proxify(obj, handler));
 }
 
 function promiseLog(promise, p1, p2) {
-    return new Promise((res, res) => {
+    return new Promise((res, rej) => {
         promise.then(() => {
             console.log(p1)
             res(p1)
         }).catch(() => {
             console.log(p2)
-            res(p2)
+            rej(p2)
         })
     })
 }
 
 module.exports = {
-    safe: function(obj, evalFunc, def) {
+    safe: function (obj, evalFunc, def) {
         return optional(obj, evalFunc, def)
     },
-    promise: function(p, p1, p2) {
+    promise: function (p, p1, p2) {
         return promiseLog(p, p1, p2)
     }
 }
